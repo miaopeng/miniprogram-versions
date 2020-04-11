7b2c569a0a911a55a4fb286c28cb88b912dc8da9
miaopeng <mios426@gmail.com>
2020-04-11 21:39:23 +0800

    npm-publish action

--- scripts/check.sh
+++ scripts/check.sh
@@ -4,10 +4,14 @@ if [ -z "$(git status --porcelain)" ]; then
 else 
   # Uncommitted changes
   echo 'New Update!'
-  yarn readme
   git config --local user.email "mios426@gmail.com"
   git config --local user.name "miaopeng"
+  npm run readme
   git add .
   git commit -m 'auto update'
-  git push
+  npm version patch
+  git push origin master --follow-tags
+  git checkout release
+  git merge master
+  git push origin release
 fi
