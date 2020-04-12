b3373a143387afd01e72108c4aece1601fa2fdfc
miaopeng <mios426@gmail.com>
2020-04-12 13:31:25 +0800

    fix checkout issue

--- scripts/check.sh
+++ scripts/check.sh
@@ -1,9 +1,10 @@
 if [ -z "$(git status --porcelain)" ]; then 
   # Working directory clean
-  echo 'clean'
+  echo "::set-output name=result::clean"
+
 else 
   # Uncommitted changes
-  echo 'New Update!'
+  echo "::set-output name=result::new_update"
   git config --local user.email "mios426@gmail.com"
   git config --local user.name "miaopeng"
   npm run readme
@@ -11,7 +12,9 @@ else
   git commit -m 'auto update'
   npm version patch
   git push origin master --follow-tags
+  git branch -a
   git checkout release
   git merge master
   git push origin release
+  echo "::set-output name=result::update_success"
 fi
